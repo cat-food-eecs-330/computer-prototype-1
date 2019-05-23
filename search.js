934e20568a11fbd0fab38cc5a3990f54f4b99911
kevin <kevinrachwalski2021@u.northwestern.edu>
2019-05-22 23:07:09 -0500

    added some filters to the search

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+var foodIds = ["meatball", "mspag", "spag"]
+var foodtoggels = [true, true, true, true, true]
 function filterFunction() {
   var input, filter, ul, li, a, i;
   input = document.getElementById("myInput");
@@ -6,6 +8,8 @@ function filterFunction() {
   a = div.getElementsByTagName("a");
   for (i = 0; i < a.length; i++) {
     txtValue = a[i].textContent || a[i].innerText;
+    console.log(txtValue)
+    console.log("This ran")
     if (txtValue.toUpperCase().indexOf(filter) > -1) {
       a[i].style.display = "";
     } else {
@@ -21,4 +25,34 @@ function click0() {
   showDrpDwn();
   document.getElementById("0").style.display = "";
   localStorage.lastLocaleSelection = "0";
+}
+function click1() {
+  console.log(foodtoggels[1])
+  if (foodtoggels[1] == true) {
+      document.getElementById(foodIds[1]).style.display = "none"
+      document.getElementById(foodIds[0]).style.display = "none"
+      foodtoggels[1] = false
+  } else {
+    if (foodtoggels[2] == true) {
+    console.log("THIS WAS CALLEDg")
+  document.getElementById(foodIds[1]).style.display = "block"
+  document.getElementById(foodIds[0]).style.display = "block"
+
+    }
+      foodtoggels[1] = true
+  }
+}
+function click2() {
+  console.log("THIS WAS CALLED")
+  if (foodtoggels[2] == true && foodtoggels[1] == true) {
+      document.getElementById(foodIds[1]).style.display = "none"
+      document.getElementById(foodIds[0]).style.display = "none"
+      foodtoggels[2] = false
+  } else {
+    if (foodtoggels[1] == true) {
+  document.getElementById(foodIds[1]).style.display = "block"
+  document.getElementById(foodIds[0]).style.display = "block"
+  foodtoggels[2] = true
+    }
+  }
 }
